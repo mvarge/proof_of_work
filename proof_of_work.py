86b50b17bde9e2b40cb60ba6483b12f7cbc09c7d
mvarge <marcelo.varge@gmail.com>
2018-01-02 05:52:40 +0000

    correcting character addition as string's suffix

--- proof_of_work.py
+++ proof_of_work.py
@@ -5,15 +5,16 @@ import argparse
 def proof_of_work(string, level=1, verbose=False):
     count = 0
     while True:
-        for x in range(0, 256):
-            string += chr(x)
-            h = hashlib.sha256(b'{0}'.format(string)).hexdigest()
-            count += 1
-            if verbose:
-                print("Try: {0} - {1}".format(count, h))
-            if h[0:level] == ('0' * level):
-                print("Coin costed {0} tests.".format(count))
-                return
+        for y in range(0, 256):
+            for x in range(0, 256):
+                h = hashlib.sha256(b'{0}{1}'.format(string, chr(x))).hexdigest()
+                count += 1
+                if verbose:
+                    print("Try: {0} - {1}".format(count, h))
+                if h[0:level] == ('0' * level):
+                    print("Coin costed {0} tests.".format(count))
+                    return
+            string = "{0}{1}".format(string, chr(y))
 
 def parse():
     parser = argparse.ArgumentParser()
